520b3c1c928e968f0a56e22fd6fd175181a5da5a
thuriabdullah <thiii302081@gmail.com>
2021-10-29 11:46:20 +0300

    lab

--- advanced.js
+++ advanced.js
@@ -1,19 +1,43 @@
-function printName(fristname){
-   return (fristname)
-}
-printName();
+// function printName(fristName){
+//    return"my name   "+fristName
+   
+// }
+// document.getElementById("test").innerHTML=printName("dalal");
+
+
 
 //arrowfunction
-// let printName=(fristname)=>fristname
-  
-// printName();
+  let printName=(fristName)=> "my name   "+fristName
+
+document.getElementById("test").innerHTML=printName("dalal");
+
 
 
 //display time
 function displayTime(){
    let currentDateTime=new Date();
-   document.querySelector("#time").innerHTML=(currentDateTime)
+   document.querySelector("#time").innerHTML = "time" + currentDateTime
+  // return "time"+currentDateTime
 }
+
+// console.log(displayTime());
+ //setInterval(displayTime(),1000)
+
+//Date & Time
+function showTime() {
+   const date = new Date();
+   let day = date.getDate();
+   let month = date.getMonth() + 1;
+   let year = date.getFullYear();
+   let time = date.toLocaleTimeString();
+
+   let currentDate = `${day}-${month}-${year}`
+
+   document.querySelector("#time").innerText = time;
+   setInterval(showTime, 1000);
+}
+showTime();
+
    
-   setInterval(displayTime,1000)
+   
 
